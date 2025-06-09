68d675648749dd0a9b08100a4f676661534a053b
vaishnavi <vishu032004@gmail.com>
2025-06-09 11:43:20 +0530

    little setup

---
+++ src/app/Components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <>
+   <div class="flex flex-nowrap">
+  <div>01</div>
+  <div>02</div>
+  <div>03</div>
+</div>
+    </>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Main from './Components/Main';
+
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-800">
-      <h1 className="text-2xl font-bold text-red-500">
-        Tailwind Colors Test 🎨
-      </h1>
+    <Main></Main>
     </div>
   );
 }
