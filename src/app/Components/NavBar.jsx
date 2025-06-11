41d75ef9c1e1c7c4438eca6201f507de3f1e6cbd
vaishnavi <vishu032004@gmail.com>
2025-06-11 21:23:19 +0530

    two img

--- src/app/Components/NavBar.jsx
+++ src/app/Components/NavBar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div >
-      
+    <div className='bg-[url(/BGmm.png)] h-screen w-[460px]  flex flex-col ...'>
+       <div>
+        <img src="/iCon/Main.png" alt="Logo" className='w-20 h-20 mx-auto mt-10' />
+       </div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Main from './Components/Main';
+import NavBar from './Components/NavBar';
 
 
 export default function Home() {
   return (
     <div className="flex ...">
-  <div className="size-14 flex-none ...">01</div>
+  <div className="size-14 flex-none ..."><NavBar></NavBar></div>
   <div className="size-14 grow ...">
     <Main></Main>
   </div>
