d1e93e57052416ddabe58d5c5cb04306faac9a6f
vaishnavi <vishu032004@gmail.com>
2025-06-14 17:35:15 +0530

    two img

--- src/app/Components/Small.jsx
+++ src/app/Components/Small.jsx
@@ -4,7 +4,7 @@ export default function Small() {
   return (
     <div className='bg-[#FFFFFF] w-[274px] h-[240px] ml-[23px] mt-[289px] rounded-[85px] position: relative'>
      <div className='bg-[#040202] w-[50px] h-[50px] rounded-full mt-[20px] ml-[180px] position: absolute'>
-
+      <img src='./iCon/tag-2.png' alt='Vector' className='w-[27px] h-[27px] mt-[13px] ml-[12px]' />
      </div>
     </div>
   )
