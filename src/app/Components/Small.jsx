5d38b4afe93ea93b51e97b9e1a9130e546efbae7
vaishnavi <vishu032004@gmail.com>
2025-06-14 17:27:26 +0530

    little change

--- src/app/Components/Small.jsx
+++ src/app/Components/Small.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Small() {
   return (
-    <div className='bg-[#FFFFFF] w-[274px] h-[240px] ml-[23px] mt-[289px] rounded-[85px]'>
-     
+    <div className='bg-[#FFFFFF] w-[274px] h-[240px] ml-[23px] mt-[289px] rounded-[85px] position: relative'>
+     <div className='bg-[#040202] w-[50px] h-[50px] rounded-full mt-[20px] ml-[180px] position: absolute'>
+
+     </div>
     </div>
   )
 }
