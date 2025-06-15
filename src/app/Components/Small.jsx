ad19b3f0401ccc824f8acce76968e46d525cbc4a
vaishnavi <vishu032004@gmail.com>
2025-06-15 14:09:35 +0530

    font added

--- src/app/Components/Small.jsx
+++ src/app/Components/Small.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 export default function Small() {
   return (
     <div className='bg-[#FFFFFF] w-[274px] h-[240px] ml-[23px] mt-[289px] rounded-[85px] position: relative'>
-     <div className='bg-[#040202] w-[50px] h-[50px] rounded-full mt-[20px] ml-[180px] position: absolute'>
-      <img src='./iCon/tag-2.png' alt='Vector' className='w-[27px] h-[27px] mt-[13px] ml-[12px]' />
-     </div>
+
+      <div className="flex flex-col ...">
+        <div className='border-[#D5D5EB] border-[1px] w-[85px] h-[43px] rounded-[64px]  ml-[39px] mt-[40px] flex items-center justify-center font-weight-[500] font-size-[16px] paddding-[16px] fon'>
+          <div className='... font-outfit'>
+  20% off
+</div>
+
+        </div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+      {/* <div className='bg-[#040202] w-[50px] h-[50px] rounded-full mt-[10px] ml-[180px] position: absolute'>
+        <img src='./iCon/tag-2.png' alt='Vector' className='w-[27px] h-[27px] mt-[13px] ml-[12px]' />
+      </div> */}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
