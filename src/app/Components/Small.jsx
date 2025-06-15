00f4488d199a4a5347278fc38457472825243c38
vaishnavi <vishu032004@gmail.com>
2025-06-15 14:17:47 +0530

    two box done

--- src/app/Components/Small.jsx
+++ src/app/Components/Small.jsx
@@ -5,13 +5,17 @@ export default function Small() {
     <div className='bg-[#FFFFFF] w-[274px] h-[240px] ml-[23px] mt-[289px] rounded-[85px] position: relative'>
 
       <div className="flex flex-col ...">
-        <div className='border-[#D5D5EB] border-[1px] w-[85px] h-[43px] rounded-[64px]  ml-[39px] mt-[40px] flex items-center justify-center font-weight-[500] font-size-[16px] paddding-[16px] fon'>
+        <div className='border-[#D5D5EB] border-[1px] w-[85px] h-[43px] rounded-[64px]  ml-[39px] mt-[40px] flex items-center justify-center font-weight-[500] font-size-[16px] paddding-[16px] '>
           <div className='... font-outfit'>
   20% off
 </div>
 
         </div>
-        <div>02</div>
+       <div className='border-[#D5D5EB] border-[1px] w-[128px] h-[43px] rounded-[64px]  ml-[39px] mt-[83px] flex items-center justify-center font-weight-[700] font-size-[16px] paddding-[16px] position: absolute'>
+          <div className='... font-outfit'>
+  Free Popcorn
+</div>
+</div>
         <div>03</div>
       </div>
       {/* <div className='bg-[#040202] w-[50px] h-[50px] rounded-full mt-[10px] ml-[180px] position: absolute'>
