524107a4a00840500c875155c64c6c23a5f15817
vaishnavi <vishu032004@gmail.com>
2025-06-16 10:45:26 +0530

    small area completed

--- src/app/Components/Small.jsx
+++ src/app/Components/Small.jsx
@@ -4,20 +4,20 @@ export default function Small() {
   return (
     <div className='bg-[#FFFFFF] w-[274px] h-[240px] ml-[23px] mt-[289px] rounded-[85px] position: relative'>
 
-      <div className="flex flex-col ...">
-        <div className='border-[#D5D5EB] border-[1px] w-[85px] h-[43px] rounded-[64px]  ml-[39px] mt-[40px] flex items-center justify-center font-weight-[500] font-size-[16px] paddding-[16px] '>
+      <div className="flex flex-col ... ">
+        <div className=' bg-[#FFFFFF] border-[#D5D5EB] border-[1px] w-[80px] h-[30px] rounded-[64px]  ml-[39px] mt-[23px] flex items-center justify-center font-weight-[500] font-size-[6px] paddding-[12px] '>
           <div className='... font-outfit'>
   20% off
 </div>
 
         </div>
         <div className='flex flex-row'>
-<div className='border-[#D5D5EB] border-[1px] w-[128px] h-[43px] rounded-[64px]  ml-[39px] mt-[0px] flex items-center justify-center font-weight-[700] font-size-[16px] paddding-[16px] position: absolute'>
+<div className='border-[#D5D5EB] bg-[#FFFFFF] border-[1px] w-[128px] h-[35px] rounded-[64px]  ml-[39px] mt-[0px] flex items-center justify-center font-weight-[700] font-size-[10px] paddding-[16px] position: absolute'>
           <div className='... font-outfit'>
   Free Popcorn
 </div>
 </div>
-        <div className='  bg-[#FFFFFF]   transform rotate-12 border-[#D5D5EB] border-[1px] w-[75px] h-[43px] rounded-[64px]  ml-[160px] mt-[5px] flex items-center justify-center font-weight-[700] font-size-[16px] paddding-[16px] position: absolute'>
+        <div className='  bg-[#FFFFFF]   transform rotate-12 border-[#D5D5EB] border-[1px] w-[75px] h-[35px] rounded-[64px]  ml-[160px] mt-[5px] flex items-center justify-center font-weight-[700] font-size-[10px] paddding-[16px] position: absolute'>
           <div className='... font-outfit'>
  5% off
 </div>
@@ -28,6 +28,25 @@ export default function Small() {
       {/* <div className='bg-[#040202] w-[50px] h-[50px] rounded-full mt-[10px] ml-[180px] position: absolute'>
         <img src='./iCon/tag-2.png' alt='Vector' className='w-[27px] h-[27px] mt-[13px] ml-[12px]' />
       </div> */}
+
+      <div className="flex flex-col ...">
+ 
+<div className='... font-outfit text-[#8A8AAA] mt-[48px] text-[24px] ml-[40px] '>
+ Promo
+
+  </div>
+  
+  <div className='flex flex-row'>
+<div className='... font-outfit text-[#000000] mt-[2pxpx] text-[48px] ml-[40px] '>
+ 24
+
+  </div>
+  <div className='... font-outfit text-[#8A8AAA] mt-[8px] text-[17px] ml-[3px] '>
+ ready
+
+  </div>
+  </div>
+</div>
     </div>
   )
 }
