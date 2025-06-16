bc4797df37db08db22c7a2d40ac94ddfd5b88e61
vaishnavi <vishu032004@gmail.com>
2025-06-16 10:53:46 +0530

    ADDED IMAGE

--- src/app/Components/Main.jsx
+++ src/app/Components/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Main() {
   return (
     <>
-     <div className="bg-[url('/BG_IMG.png')] h-screen bg-no-repeat bg-[length:69%_auto] bg-[position:100px_center]  ml-[40px] rounded-[80px]">
+     <div className="bg-[url('/BG.png')] h-screen bg-no-repeat bg-[length:69%_auto] bg-[position:100px_center]  ml-[40px] rounded-[80px]">
         {/* You can add some content here */}
         
      </div>
