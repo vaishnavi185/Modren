139750272be8537854aa546bfb71006831c79470
vaishnavi <vishu032004@gmail.com>
2025-06-17 21:25:06 +0530

    tp

--- src/app/Components/NavBar.jsx
+++ src/app/Components/NavBar.jsx
@@ -9,11 +9,11 @@ export default function NavBar() {
     <div className='bg-[url(/NavBg.png)] bg-no-repeat bg-contain bg-left min-h-screen w-[460px] flex flex-col m-0 p-0 position: absolute'>
        <div>
         <img src="/iCon/Main.png" alt="Logo" className='w-20 h-20 mx-auto mt-[37px] ml-[30px]' />
-        
+         
        </div>
 
 
-       <div className="flex flex-col ... mt-[30px] ml-[49px] gap-[20px] ">
+       <div className="flex flex-col  mt-[30px] ml-[49px] gap-[20px] ">
   <div>
     <img src="/iCon/home-2.png" alt="Logo" className='w-20 h-20 mx-auto  ' />
   </div>
