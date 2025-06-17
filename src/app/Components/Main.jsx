ae99968a7c440c091535662addf08fe90c250484
vaishnavi <vishu032004@gmail.com>
2025-06-17 20:36:06 +0530

    bg setting

--- src/app/Components/Main.jsx
+++ src/app/Components/Main.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Main() {
   return (
     <>
-    <div className="bg-[url('/BG.png')] bg-no-repeat      bg-[center_-10px] bg-cover ">
+   <div className="bg-[url('/BG.png')] bg-no-repeat bg-cover h-screen  bg-[center_5px] bg-[right-20px] rounded-[90px] rounded-bl-[520px] rounded-tl-[400px]">
+
   {/* You can add some content here */}
 
 
