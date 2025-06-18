34cf684dedcc30990dc8c5ca088abff10186cb32
vaishnavi <vishu032004@gmail.com>
2025-06-18 10:40:23 +0530

    tp

--- src/app/Components/Main.jsx
+++ src/app/Components/Main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import TopNav from './TopNav';
 
 export default function Main() {
   return (
     <>
-   <div className="bg-[url('/BG.png')] bg-no-repeat bg-cover h-screen  bg-[center_5px] bg-[right-20px] rounded-[90px] rounded-bl-[520px] rounded-tl-[400px]">
-
-  {/* You can add some content here */}
-
-
-</div>
-
+      <div className="bg-[url('/BG.png')] bg-no-repeat bg-cover h-screen 
+        bg-[right_20px_top_5px] 
+        rounded-[90px] rounded-bl-[790px] rounded-tl-[600px]
+      ">
+        <TopNav></TopNav>
+        {/* Add your content here */}
+      </div>
     </>
-  )
+  );
 }
--- src/app/Components/TopNav.jsx
+++ src/app/Components/TopNav.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function TopNav() {
   return (
-    <div>
-      
-    </div>
+  <div>hello</div>
   )
 }
