5738490dd5ffec61cb28a9a7b6cb164dadf2b4d9
vaishnavi <vishu032004@gmail.com>
2025-06-19 11:21:47 +0530

    img top nav set

--- src/app/Components/Main.jsx
+++ src/app/Components/Main.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import TopNav from './TopNav';
-
-export default function Main() {
+export default function StyledImage() {
   return (
-    <>
-      <div className="bg-[url('/BG.png')] bg-no-repeat bg-cover h-screen 
-        bg-[right_20px_top_5px] 
-        rounded-[90px] rounded-bl-[790px] rounded-tl-[600px]
-      ">
-        <TopNav></TopNav>
-        {/* Add your content here */}
-      </div>
-    </>
+    <div className="w-[1200px] h-[900px] md:w-[400px] md:h-[400px] overflow-hidden ml-[120px] ">
+      <img
+        src="BG.png"
+        alt="Styled character"
+        className="w-full h-full object-cover transition-all duration-300 ease-in-out [clip-path:polygon(2%_0%,_2%_50%,_20%_64%,_27%_67%,_27%_94%,_98%_95%,_99%_0%,_78%_0%,_68%_12%,_44%_12%,_34.5%_0%)]"
+      />
+    </div>
   );
 }
--- src/app/Components/TopNav.jsx
+++ src/app/Components/TopNav.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function TopNav() {
   return (
-  <div>hello</div>
-  )
+    <div className="h-[150px] w-[500px] bg-[#F5F5FC] relative flex flex-row  items-center blur-[10px] mt-[2px] rounded-[25px] align-middle">
+      hdhckdj
+    </div>
+  );
 }
