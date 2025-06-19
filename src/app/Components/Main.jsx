2da6dcf97ccb4bf244e9afc769554a70c0da7854
vaishnavi <vishu032004@gmail.com>
2025-06-19 12:03:16 +0530

    top nav

--- src/app/Components/Main.jsx
+++ src/app/Components/Main.jsx
@@ -1,11 +1,20 @@
-export default function StyledImage() {
+import TopNav from "./TopNav";
+
+export default function Main() {
   return (
-    <div className="w-[1200px] h-[900px] md:w-[400px] md:h-[400px] overflow-hidden ml-[120px] ">
+    <div className="relative w-[1200px] h-[900px] md:w-[400px] md:h-[400px] overflow-hidden ml-[120px]">
+      
+      {/* Image with polygon clip-path */}
       <img
         src="BG.png"
         alt="Styled character"
-        className="w-full h-full object-cover transition-all duration-300 ease-in-out [clip-path:polygon(2%_0%,_2%_50%,_20%_64%,_27%_67%,_27%_94%,_98%_95%,_99%_0%,_78%_0%,_68%_12%,_44%_12%,_34.5%_0%)]"
+        className=" absolute w-full h-full object-cover transition-all duration-300 ease-in-out [clip-path:polygon(2%_0%,_2%_50%,_20%_64%,_27%_67%,_27%_94%,_98%_95%,_99%_0%,_78%_0%,_68%_12%,_44%_12%,_34.5%_0%)]"
       />
+
+      {/* TopNav on top of the image */}
+      <div className="absolute ">
+        <TopNav />
+      </div>
     </div>
   );
 }
--- src/app/Components/TopNav.jsx
+++ src/app/Components/TopNav.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-
 export default function TopNav() {
   return (
-    <div className="h-[150px] w-[500px] bg-[#F5F5FC] relative flex flex-row  items-center blur-[10px] mt-[2px] rounded-[25px] align-middle">
-      hdhckdj
+    <div className="h-[70px] bg-[#F5F5FC] flex items-center justify-center rounded-[50px] ml-[540px]  mt-[20px] w-[270px] flex flex-row gap-[10px]">
+
+<text className="text-[#8A8AAB] text-[20px]">32</text>
+<img src="iCon/ticket.png" className="size-[30px]" >
+
+    </img>
+     <text className="text-[#8A8AAB] text-[20px]">65</text>
+    <img src="iCon/wallet.png" className="size-[30px]" >
+
+    </img>
+
+      <img  src="iCon/Bell.png " className="size-[35px]">
+    </img>
     </div>
   );
 }
