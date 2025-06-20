11283d8c2354d57d30f6a346267ed48860da74ee
vaishnavi <vishu032004@gmail.com>
2025-06-20 09:07:17 +0530

    topnav done

--- src/app/Components/TopNav.jsx
+++ src/app/Components/TopNav.jsx
@@ -1,18 +1,27 @@
 export default function TopNav() {
   return (
-    <div className="h-[70px] bg-[#F5F5FC] flex items-center justify-center rounded-[50px] ml-[540px]  mt-[20px] w-[270px] flex flex-row gap-[10px]">
+    <div className="h-[70px] bg-[#F5F5FC] flex items-center justify-center rounded-[50px] ml-[540px] mt-[20px] w-[270px] flex-row gap-[20px]">
+      
+      {/* Ticket Section */}
+      <div className="flex flex-row items-center gap-[6px]">
+        <span className="text-[#8A8AAB] text-[18px] mt-[3px]">32</span>
+        <img src="iCon/ticket.png" className="size-[30px]" />
+      </div>
 
-<text className="text-[#8A8AAB] text-[20px]">32</text>
-<img src="iCon/ticket.png" className="size-[30px]" >
+      {/* Vertical Dashed Line */}
+      <div className=" h-[15px] border-r-2 border-dashed border-[#8A8AAB] "></div>
 
-    </img>
-     <text className="text-[#8A8AAB] text-[20px]">65</text>
-    <img src="iCon/wallet.png" className="size-[30px]" >
+      {/* Wallet Section */}
+      <div className="flex flex-row items-center gap-[6px]">
+        <span className="text-[#8A8AAB] text-[18px] mt-[3px]">65</span>
+        <img src="iCon/wallet.png" className="size-[30px]" />
+      </div>
 
-    </img>
+      {/* Vertical Dashed Line */}
+      <div className=" h-[15px] border-r-2 border-dashed border-[#8A8AAB]"></div>
 
-      <img  src="iCon/Bell.png " className="size-[35px]">
-    </img>
+      {/* Bell Icon */}
+      <img src="iCon/Bell.png" className="size-[35px]" />
     </div>
   );
 }
