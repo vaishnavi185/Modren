b0e4695136fd4a7c9a62c28c1137d484d5629ada
vaishnavi <vishu032004@gmail.com>
2025-06-24 10:15:14 +0530

    box width done

--- src/app/Components/RightComp.jsx
+++ src/app/Components/RightComp.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function RightComp() {
   return (
-    <div className='w-[400px] h-[600px] bg-[#F5F5FC] rounded-[110px] absolute left-[1317px]'>
+    <>
+    <div className='w-[320px] h-[624px] bg-[#F5F5FC] rounded-[110px] absolute left-[1317px]'>
       <div
-        className='w-[380px] h-[580px] bg-[#FFFFFF] absolute top-[10px] left-[10px] rounded-[110px]'
+        className='w-[304px] h-[610px] bg-[#FFFFFF] absolute top-[10px] left-[10px] rounded-[110px]'
         style={{
           clipPath: `path('
             M 100 0
@@ -28,32 +29,37 @@ export default function RightComp() {
 
         <div className='flex flex-row '>
 
-          <img src='/iCon/location.png' className='size-[20px] ml-[240px] mt-[68px]'>
+          <img src='/iCon/location.png' className='size-[20px] ml-[180px] mt-[68px]'>
           </img>
-        <div className='font-outfit text-[14px] font-bold text-[#8A8AAB]  ml-[10px] mt-[60px]'>
+        <div className='font-outfit text-[12px] font-bold text-[#8A8AAB]  ml-[10px] mt-[60px]'>
   Cy cinema,<br />23 Azumaki St.
 </div>
 
 
 
         </div>
-        <div className="flex gap-[10px] mt-[20px] ml-[30px]">
+        <div className="flex gap-[10px] mt-[20px] ml-[10px]">
   {/* Selected Time */}
- <div className="w-[90px] h-[40px] rounded-full bg-[#000000] text-[#FFFFFF] text-[16px] font-medium flex items-center justify-center">
+ <div className="w-[80px] h-[40px] rounded-full bg-[#000000] text-[#FFFFFF] text-[16px] font-medium flex items-center justify-center">
   18:00
 </div>
 
 
   {/* Unselected Times */}
-  <div className=" font-outfit w-[90px] h-[40px] rounded-full border-2 border-[#C2C2E2] text-[#0E0E2C] text-[16px] justify-center font-medium flex items-center justify-center">
+  <div className=" font-outfit w-[80px] h-[40px] rounded-full border-2 border-[#C2C2E2] text-[#0E0E2C] text-[16px] justify-center font-medium flex items-center justify-center">
     21:25
   </div>
-  <div className="  font-outfit w-[90px] h-[40px] rounded-full border-2 border-[#C2C2E2] text-[#0E0E2C] text-[16px] justify-center font-medium flex items-center justify-center">
+  <div className="  font-outfit w-[80px] h-[40px] rounded-full border-2 border-[#C2C2E2] text-[#0E0E2C] text-[16px] justify-center font-medium flex items-center justify-center">
     23:50
   </div>
 </div>
 
       </div>
+
     </div>
+    <div className='w-[320px] h-[287px] bg-[#000000] rounded-[88px] absolute top-[639px] left-[1310px]'>
+
+    </div>
+    </>
   );
 }
